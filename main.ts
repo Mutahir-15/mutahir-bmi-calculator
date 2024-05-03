7d3246d52afbfd101fc676ce5b350b830e2664c2
Mutahir-15 <164089040+Mutahir-15@users.noreply.github.com>
2024-05-03 15:57:21 +0500

    Project BMI Calculator

---
+++ main.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+
+const weightInKg = await inquirer.prompt ([
+{
+    name : "wkg",
+    type: "number",
+    message: "Enter your Weight in KG: "
+}
+]);
+const heightInMeter = await inquirer.prompt([
+{
+    name: "hm",
+    type: "number",
+    message: "Enter your Height in Meters: "
+}
+]);
+if (weightInKg.wkg >= 1) {
+    let bmi = weightInKg.wkg /(heightInMeter.hm **2);
+    console.log(`Your BMI is: ${bmi}`)
+}
+else (
+    console.log("Incorrect value.")
+)
